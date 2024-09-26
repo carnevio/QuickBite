b8c0cb1ad0c1941602faaa5da3a15a0d51c2ed0c
JanisHuber <janis.huber@itme.ch>
2024-09-26 14:42:16 +0200

    1.1.8
    Try3

--- API/server.js
+++ API/server.js
@@ -2,9 +2,13 @@ import express from 'express';
 import fetch from 'node-fetch';
 import cors from 'cors';
 
+import dotenv from 'dotenv'; 
+
+dotenv.config();
+
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
